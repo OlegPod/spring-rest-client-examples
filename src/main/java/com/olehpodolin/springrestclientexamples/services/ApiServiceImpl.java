91db49272d9072b021dfb0981b1e7ae0268e7118
OlegPod <kepacuh@gmail.com>
2019-08-10 19:23:32 +0300

    Using Spring WebClient instead of REST Templates

--- src/main/java/com/olehpodolin/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/com/olehpodolin/springrestclientexamples/services/ApiServiceImpl.java
@@ -3,9 +3,13 @@ package com.olehpodolin.springrestclientexamples.services;
 import com.olehpodolin.api.domain.User;
 import com.olehpodolin.api.domain.UserData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -16,7 +20,7 @@ public class ApiServiceImpl implements ApiService {
 
     private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api_url}") String api_url) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
         this.api_url = api_url;
     }
@@ -31,5 +35,17 @@ public class ApiServiceImpl implements ApiService {
         UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 }
